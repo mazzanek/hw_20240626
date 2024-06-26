2e663b8050d1d726717d71cd8e74ddea8f2f014e
mazzanek <158159078+mazzanek@users.noreply.github.com>
2024-06-26 11:37:44 +0200

    step2

--- city/structure/urls.py
+++ city/structure/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin_site/<str:title>', views.main_admin, name='admin_site'),
+    path('contact/<str:title>', views.main_contact, name='contact'),
+    path('facts/<str:title>', views.main_facts, name='facts'),
+    path('/<str:title>', views.main_page, name='main'),
+    path('news/<str:title>', views.news_page, name='news'),
+    path('history/people/<str:title>', views.history_page, name='history'),
+    path('history/photos/<str:title>', views.history_page, name='history'),
+
     path('admin_site/', views.main_admin, name='admin_site'),
     path('contact/', views.main_contact, name='contact'),
     path('facts/', views.main_facts, name='facts'),
@@ -10,7 +18,7 @@ urlpatterns = [
     path('history/', views.history_page, name='history'),
     path('history/people', views.people_page, name='people'),
     path('history/photos', views.photos_page, name='photos'),
-
-
-
 ]
+
+""" TODO: nefunguje mi toto omezení na hlavní stránku a na history site - takze stranky
+   http://127.0.0.1:8000/history/blbost nebo http://127.0.0.1:8000/blbost vykazuji chybu"""
--- city/structure/views.py
+++ city/structure/views.py
@@ -1,32 +1,60 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
 def main_page(request):
-  template = loader.get_template('main.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
 
-def news_page(request):
-  template = loader.get_template('news.html')
-  return HttpResponse(template.render())
 
-def main_admin(request):
-  template = loader.get_template('admin_site.html')
-  return HttpResponse(template.render())
+def news_page(request, title=None):
+  if title is None:
+    template = loader.get_template('news.html')
+    return HttpResponse(template.render())
+  else:
+    template = loader.get_template('news.html')
+    return HttpResponse(template.render())
 
-def main_facts(request):
-  template = loader.get_template('facts.html')
-  return HttpResponse(template.render())
+def main_admin(request, title=None):
+  if title is None:
+    template = loader.get_template('admin_site.html')
+    return HttpResponse(template.render())
+  else:
+    template = loader.get_template('admin_site.html')
+    return HttpResponse(template.render())
+
+def main_facts(request, title=None):
+  if title is None:
+    template = loader.get_template('facts.html')
+    return HttpResponse(template.render())
+  else:
+    template = loader.get_template('facts.html')
+    return HttpResponse(template.render())
 
 def main_contact(request):
   template = loader.get_template('contact.html')
   return HttpResponse(template.render())
 
-def history_page(request):
-  template = loader.get_template('history.html')
-  return HttpResponse(template.render())
-def people_page(request):
-  template = loader.get_template('people.html')
-  return HttpResponse(template.render())
-def photos_page(request):
-  template = loader.get_template('photos.html')
-  return HttpResponse(template.render())
+def history_page(request, title=None):
+  if title is None:
+    template = loader.get_template('history.html')
+    return HttpResponse(template.render())
+  else:
+    template = loader.get_template('history.html')
+    return HttpResponse(template.render())
+
+def people_page(request, title=None):
+  if title is None:
+    template = loader.get_template('people.html')
+    return HttpResponse(template.render())
+  else:
+    template = loader.get_template('people.html')
+    return HttpResponse(template.render())
+
+def photos_page(request,title=None):
+  if title is None:
+    template = loader.get_template('photos.html')
+    return HttpResponse(template.render())
+  else:
+    template = loader.get_template('photos.html')
+    return HttpResponse(template.render())
